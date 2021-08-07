918c7f12308b582b3985155cec04c11231f4979a
milky5 <sakurasketch@zoho.com>
2021-08-07 16:35:18 +0900

    can file create and append text

--- PushToGitHub/Program.cs
+++ PushToGitHub/Program.cs
@@ -13,15 +13,20 @@ namespace PushToGitHub
                 // H:\push-to-github\PushToGitHub\bin\Debug\netcoreapp3.1
                 Console.WriteLine(Directory.GetCurrentDirectory());
                 string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "tmp");
-                
-                // Clone(folderPath);
+
+                Clone(folderPath);
+
+                DateTime payDate = DateTime.Now;
+                string targetFileName = payDate.ToString("yyyyMM") + ".csv";
+                string targetFilePath = Path.Combine(folderPath, targetFileName);
+
+                AppendFile(targetFilePath, "new data");
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
                 throw;
             }
-
 
             Console.WriteLine("success");
             Console.Read();
@@ -50,6 +55,27 @@ namespace PushToGitHub
                 Console.WriteLine($"error: {ex}");
                 throw;
             }
+        }
+
+        public static void AppendFile(string path, string text)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    File.Create(path).Dispose();
+                }
+
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(text);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
         }
     }
 }
