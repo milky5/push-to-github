5334de94aaca7ce641019c923642a2d819dcd027
milky5 <sakurasketch@zoho.com>
2021-08-07 23:22:02 +0900

    can push include comment

--- PushToGitHub/Program.cs
+++ PushToGitHub/Program.cs
@@ -12,7 +12,7 @@ namespace PushToGitHub
             {
                 // H:\push-to-github\PushToGitHub\bin\Debug\netcoreapp3.1
                 Console.WriteLine(Directory.GetCurrentDirectory());
-                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "tmp");
+                string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "tmp" + DateTime.Now.ToString("hhmmss"));
 
                 Clone(folderPath);
 
@@ -21,6 +21,8 @@ namespace PushToGitHub
                 string targetFilePath = Path.Combine(folderPath, targetFileName);
 
                 AppendFile(targetFilePath, "new data");
+
+                Commit(folderPath, targetFilePath);
             }
             catch (Exception ex)
             {
@@ -36,8 +38,6 @@ namespace PushToGitHub
         {
             try
             {
-                Repository repo = new Repository();
-
                 string path = Environment.GetEnvironmentVariable("REPO_PATH");
 
                 CloneOptions options = new CloneOptions();
@@ -75,7 +75,64 @@ namespace PushToGitHub
             {
                 throw;
             }
+        }
 
+        public static void Commit(string folderPath, string filePath)
+        {
+            try
+            {
+                Repository repo = new Repository(folderPath);
+                //repo.Index.Add()
+                Commands.Stage(repo, filePath);
+
+                Signature signature = new Signature("kakeibo-csv", "system@example.com", DateTimeOffset.Now);
+                var commit = repo.Commit("append data", signature, signature);
+                //Console.WriteLine(commit.Author.When.DateTime);
+                //Console.WriteLine(commit.Sha);
+                //Console.WriteLine("----------------------------------");
+
+                PushOptions options = new PushOptions();
+                options.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials
+                {
+                    Username = Environment.GetEnvironmentVariable("PAT"), // access token
+                    Password = string.Empty
+                };
+                //options.OnPushStatusError = (e) => { 
+                //    Console.WriteLine(e);
+                //    Environment.Exit(1);
+                //};
+                //options.OnPushTransferProgress = (a, b, c) => {
+                //    var msg = string.Format("Transfer status: {0} {1} {2}", a, b, c);
+                //    Console.WriteLine(msg);
+                //    return true;
+                //};
+                //options.OnPackBuilderProgress = (a, b, c) =>
+                //{
+                //    var msg = string.Format("Builder status: {0} {1} {2}", a, b, c);
+                //    Console.WriteLine(msg);
+                //    return true;
+                //};
+
+                //foreach (Commit c in repo.Head.TrackedBranch.Commits)
+                //{
+                //    Console.WriteLine(c.Author.When.DateTime);
+                //    Console.WriteLine(c.Sha);
+                //}
+
+                //foreach (var item in repo.Branches)
+                //{
+                //    Console.WriteLine(item.FriendlyName);
+                //}
+
+                //Console.WriteLine(repo.Head.FriendlyName);
+
+
+                repo.Network.Push(repo.Head, options);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
